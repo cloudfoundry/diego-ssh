7f11a375ec65a5cd491f531f55dd77e8c6551075
Matthew Sykes <matthew.sykes@gmail.com>
2015-04-14 16:49:40 -0400

    Add DiegoPasswordAuthenticator
    [#90430500]
    
    Signed-off-by: Atul Kshirsagar <atul.kshirsagar@gmail.com>

---
+++ authenticators/diego_password_authenticator.go
@@ -0,0 +1,83 @@
+package authenticators
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/cloudfoundry-incubator/receptor"
+	"github.com/pivotal-golang/lager"
+	"golang.org/x/crypto/ssh"
+)
+
+type DiegoPasswordAuthenticator struct {
+	logger         lager.Logger
+	receptorCreds  []byte
+	receptorClient receptor.Client
+}
+
+var UserRegex *regexp.Regexp = regexp.MustCompile(`diego:(.*)/(\d+)`)
+
+var InvalidDomainErr error = errors.New("Invalid authentication domain")
+var InvalidCredentialsErr error = errors.New("Invalid credentials")
+
+func NewDiegoPasswordAuthenticator(logger lager.Logger, receptorClient receptor.Client, receptorCreds []byte) *DiegoPasswordAuthenticator {
+	return &DiegoPasswordAuthenticator{
+		logger:         logger,
+		receptorCreds:  receptorCreds,
+		receptorClient: receptorClient,
+	}
+}
+
+func (dpa *DiegoPasswordAuthenticator) Authenticate(metadata ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
+	logger := dpa.logger.Session("authenticate")
+	logger.Info("authentication-starting")
+	defer logger.Info("authentication-finished")
+
+	if !UserRegex.MatchString(metadata.User()) {
+		logger.Error("regex-match-fail", InvalidDomainErr)
+		return nil, InvalidDomainErr
+	}
+
+	if !bytes.Equal(dpa.receptorCreds, password) {
+		logger.Error("invalid-credentials", InvalidCredentialsErr)
+		return nil, InvalidCredentialsErr
+	}
+
+	guidAndIndex := UserRegex.FindStringSubmatch(metadata.User())
+
+	processGuid := guidAndIndex[1]
+	index, err := strconv.Atoi(guidAndIndex[2])
+	if err != nil {
+		logger.Error("atoi-failed", err)
+		return nil, err
+	}
+
+	lrpResponse, err := dpa.receptorClient.ActualLRPByProcessGuidAndIndex(processGuid, index)
+	if err != nil {
+		logger.Error("get-lrp-failed", err)
+		return nil, err
+	}
+
+	return dpa.createPermissions(&lrpResponse), nil
+}
+
+func (dpa *DiegoPasswordAuthenticator) createPermissions(lrp *receptor.ActualLRPResponse) *ssh.Permissions {
+	for _, mapping := range lrp.Ports {
+		if mapping.ContainerPort == 2222 {
+			return &ssh.Permissions{
+				CriticalOptions: map[string]string{
+					"diego:process-guid":      lrp.ProcessGuid,
+					"diego:index":             fmt.Sprintf("%d", lrp.Index),
+					"diego:instance-guid":     lrp.InstanceGuid,
+					"diego:container-address": lrp.Address,
+					"diego:ssh-daemon-port":   fmt.Sprintf("%d", mapping.HostPort),
+				},
+			}
+		}
+	}
+
+	return nil
+}
---
+++ authenticators/diego_password_authenticator_test.go
@@ -0,0 +1,128 @@
+package authenticators_test
+
+import (
+	"github.com/cloudfoundry-incubator/diego-ssh/authenticators"
+	"github.com/cloudfoundry-incubator/diego-ssh/test_helpers/fake_ssh"
+	"github.com/cloudfoundry-incubator/receptor"
+	"github.com/cloudfoundry-incubator/receptor/fake_receptor"
+	"github.com/pivotal-golang/lager/lagertest"
+	"golang.org/x/crypto/ssh"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("DiegoPasswordAuthenticator", func() {
+
+	var (
+		authenticator  *authenticators.DiegoPasswordAuthenticator
+		metadata       *fake_ssh.FakeConnMetadata
+		logger         *lagertest.TestLogger
+		receptorClient *fake_receptor.FakeClient
+		receptorCreds  []byte
+
+		permissions *ssh.Permissions
+		password    []byte
+		authErr     error
+	)
+
+	BeforeEach(func() {
+		logger = lagertest.NewTestLogger("test")
+		receptorClient = new(fake_receptor.FakeClient)
+		receptorCreds = []byte("receptor-user:receptor-password")
+		authenticator = authenticators.NewDiegoPasswordAuthenticator(logger, receptorClient, receptorCreds)
+
+		metadata = &fake_ssh.FakeConnMetadata{}
+		permissions = nil
+		password = []byte{}
+	})
+
+	JustBeforeEach(func() {
+		permissions, authErr = authenticator.Authenticate(metadata, password)
+	})
+
+	Context("when a client attempts to authenticate with a user and password", func() {
+		Context("when the user name starts with 'diego:'", func() {
+			BeforeEach(func() {
+				metadata.UserReturns("diego:some-guid/0")
+				password = []byte("receptor-user:receptor-password")
+			})
+
+			It("authentictes the password against the receptor user:password", func() {
+				Ω(authErr).ShouldNot(HaveOccurred())
+			})
+		})
+
+		Context("when the user name doesn't start with 'diego:'", func() {
+			BeforeEach(func() {
+				metadata.UserReturns("dora:some-guid")
+			})
+
+			It("fails the authentication", func() {
+				Ω(authErr).Should(MatchError("Invalid authentication domain"))
+			})
+		})
+
+		Context("when the password doesn't match the receptor credentials", func() {
+			BeforeEach(func() {
+				metadata.UserReturns("diego:some-guid/0")
+				password = []byte("cf-user:cf-password")
+			})
+
+			It("fails the authentication", func() {
+				Ω(authErr).Should(MatchError("Invalid credentials"))
+			})
+		})
+	})
+
+	Context("when authentication is successful", func() {
+		var lrpResponse receptor.ActualLRPResponse
+
+		BeforeEach(func() {
+			metadata.UserReturns("diego:some-guid/0")
+			password = []byte("receptor-user:receptor-password")
+
+			lrpResponse = receptor.ActualLRPResponse{
+				ProcessGuid:  "some-guid",
+				Index:        0,
+				InstanceGuid: "some-instance-guid",
+				Address:      "1.2.3.4",
+				Ports: []receptor.PortMapping{
+					{ContainerPort: 2222, HostPort: 3333},
+				},
+			}
+
+			receptorClient.ActualLRPByProcessGuidAndIndexReturns(lrpResponse, nil)
+		})
+
+		It("gets information about the the actual lrp from the username", func() {
+			Ω(receptorClient.ActualLRPByProcessGuidAndIndexCallCount()).Should(Equal(1))
+
+			guid, index := receptorClient.ActualLRPByProcessGuidAndIndexArgsForCall(0)
+			Ω(guid).Should(Equal("some-guid"))
+			Ω(index).Should(Equal(0))
+		})
+
+		It("saves container information in the critical options of the permissions", func() {
+			expectedOptions := map[string]string{
+				"diego:process-guid":      "some-guid",
+				"diego:index":             "0",
+				"diego:instance-guid":     "some-instance-guid",
+				"diego:container-address": "1.2.3.4",
+				"diego:ssh-daemon-port":   "3333",
+			}
+			Ω(permissions).ShouldNot(BeNil())
+			Ω(permissions.CriticalOptions).Should(Equal(expectedOptions))
+		})
+
+		Context("when getting the actual LRP information fails", func() {
+			BeforeEach(func() {
+				receptorClient.ActualLRPByProcessGuidAndIndexReturns(receptor.ActualLRPResponse{}, &receptor.Error{})
+			})
+
+			It("returns the error", func() {
+				Ω(authErr).Should(Equal(&receptor.Error{}))
+			})
+		})
+	})
+})
