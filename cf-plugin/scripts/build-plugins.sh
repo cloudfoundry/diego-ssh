9bf94604a3d4bb8a35e325e0800635b4a614c55c
Jen Spinney and Michael Fraenkel <fraenkel@us.ibm.com>
2015-06-19 16:48:36 -0700

    Fix the build script to handle the split main

--- cf-plugin/scripts/build-plugins.sh
+++ cf-plugin/scripts/build-plugins.sh
@@ -4,10 +4,10 @@ set -x
 
 go get github.com/Sirupsen/logrus
 
-GOOS=linux GOARCH=386 go build -o ssh-plugin-linux-386 plugin.go
-GOOS=linux GOARCH=amd64 go build -o ssh-plugin-linux-amd64 plugin.go
-GOOS=windows GOARCH=386 go build -o ssh-plugin-win32.exe plugin.go
-GOOS=windows GOARCH=amd64 go build -o ssh-plugin-win64.exe plugin.go
-go build -o ssh-plugin-darwin-amd64 plugin.go
+GOOS=linux GOARCH=386 go build -o ssh-plugin-linux-386 .
+GOOS=linux GOARCH=amd64 go build -o ssh-plugin-linux-amd64 .
+GOOS=windows GOARCH=386 go build -o ssh-plugin-win32.exe .
+GOOS=windows GOARCH=amd64 go build -o ssh-plugin-win64.exe .
+go build -o ssh-plugin-darwin-amd64 .
 
 shasum -a1 ssh-plugin-*
