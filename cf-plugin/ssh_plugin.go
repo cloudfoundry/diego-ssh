2775bfce1c26d04cd71008401bb0479a32787df9
Nicholas Calugar <ncalugar@pivotal.io>
2015-12-31 11:11:05 -0800

    Bump cf-plugin version to 0.2.2
    [#110832166]
    
    Signed-off-by: James Myers <jmyers@pivotal.io>

--- cf-plugin/ssh_plugin.go
+++ cf-plugin/ssh_plugin.go
@@ -28,7 +28,7 @@ func (p *SSHPlugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 2,
-			Build: 1,
+			Build: 2,
 		},
 		Commands: []plugin.Command{
 			{
