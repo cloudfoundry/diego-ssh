aa89bcf62bcc33ecfef42b1e43e6fc12a06dbe81
Amin Jamali <ajamali@vmware.com>
2023-10-20 12:09:11 +0000

    Check if DLL is found instead of dir

--- bin/test.ps1
+++ bin/test.ps1
@@ -4,12 +4,12 @@ trap { $host.SetShouldExit(1) }
 Write-Host "Downloading winpty DLL"
 Add-Type -AssemblyName System.IO.Compression, System.IO.Compression.FileSystem
 $WINPTY_DIR = "C:\winpty"
-if(!(Test-Path -Path $WINPTY_DIR )) {
-    New-Item -ItemType directory -Path $WINPTY_DIR
+$env:WINPTY_DLL_DIR="$WINPTY_DIR\x64\bin"
+if(!(Test-Path -Path $env:WINPTY_DLL_DIR )) {
+    New-Item -ItemType directory -Path $WINPTY_DIR -Force
     (New-Object System.Net.WebClient).DownloadFile('https://github.com/rprichard/winpty/releases/download/0.4.3/winpty-0.4.3-msvc2015.zip', "$WINPTY_DIR\winpty.zip")
     [System.IO.Compression.ZipFile]::ExtractToDirectory("$WINPTY_DIR\winpty.zip", "$WINPTY_DIR")
 }
-$env:WINPTY_DLL_DIR="$WINPTY_DIR\x64\bin"
 
 Debug "$(gci env:* | sort-object name | Out-String)"
 
