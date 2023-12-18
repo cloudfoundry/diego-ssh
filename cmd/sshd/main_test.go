182f15a00419240122aa9ab452b341b08c3c89cd
Amelia Downs <amelia.downs@broadcom.com>
2023-12-18 20:44:01 +0000

    update error message with kex-strict-c-v00@openssh.com algorithm

--- cmd/sshd/main_test.go
+++ cmd/sshd/main_test.go
@@ -536,7 +536,7 @@ var _ = Describe("SSH daemon", func() {
 			})
 
 			It("errors when the client doesn't provide the algorithm: 'curve25519-sha256@libssh.org'", func() {
-				Expect(dialErr).To(MatchError("ssh: handshake failed: ssh: no common algorithm for key exchange; client offered: [diffie-hellman-group14-sha1 ext-info-c], server offered: [curve25519-sha256@libssh.org]"))
+				Expect(dialErr).To(MatchError("ssh: handshake failed: ssh: no common algorithm for key exchange; client offered: [diffie-hellman-group14-sha1 ext-info-c kex-strict-c-v00@openssh.com], server offered: [curve25519-sha256@libssh.org kex-strict-s-v00@openssh.com]"))
 				Expect(client).To(BeNil())
 			})
 		})
