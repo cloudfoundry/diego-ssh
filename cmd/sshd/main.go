be9a7f368b196ce4d25f542f8fa0f560907807fb
dimitarsch <96243087+DimitarSch@users.noreply.github.com>
2025-01-07 15:32:55 +0200

    Remove chacha20 cipher from cmd/sshd/main.go
    Remove chacha20 cipher from cmd/sshd/main.go file in accordance to SAP security standards in order to prevent vulnerabilities related to terapin attacks.
    Related BLI: https://jira.tools.sap/browse/CFAR-1064

--- cmd/sshd/main.go
+++ cmd/sshd/main.go
@@ -249,7 +249,7 @@ func configure(logger lager.Logger) (*ssh.ServerConfig, error) {
 	if *allowedCiphers != "" {
 		sshConfig.Config.Ciphers = strings.Split(*allowedCiphers, ",")
 	} else {
-		sshConfig.Config.Ciphers = []string{"chacha20-poly1305@openssh.com", "aes128-gcm@openssh.com", "aes256-ctr", "aes192-ctr", "aes128-ctr"}
+		sshConfig.Config.Ciphers = []string{"aes128-gcm@openssh.com", "aes256-ctr", "aes192-ctr", "aes128-ctr"}
 	}
 
 	if *allowedMACs != "" {
