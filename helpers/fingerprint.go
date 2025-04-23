b1cb7abad936f12bdf25c41a2f6a22b14dbbc5a1
Amin Jamali <amin.jamali@broadcom.com>
2025-04-23 14:46:32 +0000

    use ssh package instead

--- helpers/fingerprint.go
+++ helpers/fingerprint.go
@@ -1,8 +1,6 @@
 package helpers
 
 import (
-	"crypto/sha256"
-	"fmt"
 	"strings"
 
 	"golang.org/x/crypto/ssh"
@@ -11,10 +9,6 @@ import (
 const SHA256_FINGERPRINT_LENGTH = 95
 
 func SHA256Fingerprint(key ssh.PublicKey) string {
-	sha256sum := sha256.Sum256(key.Marshal())
-	return colonize(fmt.Sprintf("% x", sha256sum))
-}
-
-func colonize(s string) string {
-	return strings.Replace(s, " ", ":", -1)
+	value := ssh.FingerprintSHA256(key)
+	return strings.TrimPrefix("SHA256:", value)
 }
